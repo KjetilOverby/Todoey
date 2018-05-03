8c5425ccd1b47d878f7f42a14716d32081be8e91
Kjetil Øverby <kjetiloverby@kjetils-imac.local>
2018-05-03 13:58:44 +0200

    Convert to MVC design pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kjetil Øverby on 02.05.2018.
+//  Copyright © 2018 Kjetil Øverby. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+    
+    
+    
+}
